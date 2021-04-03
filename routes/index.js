f35873bd6af2de62dd657366b0e888cd07bf0ae5
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2021-04-02 21:02:22 -0700

    Added admin page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/admin', (req, res, next) => {
+  // process.env.API_URL = https://thisisanapiserver.herokuapp.com
+  fetch(process.env.API_URL + '/requests')
+    .then(result => result.json())
+    .then(json => {
+      res.render('admin', { title: 'Admin Page', requests: json });
+    });
 });
 
 module.exports = router;
